967c634c210ca1f6ee379864f4454c975bb21809
Kennedy <joh20327@umn.edu>
2023-03-19 20:12:15 -0500

    some stuff im working on

--- weatherData.py
+++ weatherData.py
@@ -4,26 +4,40 @@ from meteostat import Hourly, Point
 import matplotlib.pyplot as plt
 import pandas as pd
 import time
+from tqdm import tqdm
 
-# TODO: make api call more efficient (switch to hrly instead of entire yr?) need faster runtime
-
-
+# geolocator = Nominatim(user_agent="MyApp",timeout=10)
+# location = geolocator.geocode("Lakewood")
+# print(location)
+# location = Point(location.latitude, location.longitude, 70) # 3rd param is altitude
+# start = datetime(2006, 10, 4, hour=2)  # year month day hour = x
+# end = datetime(2006, 10, 4, hour=2)  # year month day hour = x
+# data = Hourly(location, start, end)
+# #
+# data = data.fetch()
+# print(data)
 
 def historicalHourlyTemp(date: str, stadiumName: str, day_night: str):
     start_time = time.time()
+    locations = {}
+    if stadiumName == "COL":
+        return 0
     if len(stadiumName) < 4: ## triggers if given abbreviation, turns it into a stadium name
-        df = pd.read_csv("../minnemudac github/2023_MLBSchedule.csv")
-        for index, row in df.iterrows():
-            if row["home_team"] == stadiumName:
-                stadiumName = row["stadium_name"]
-                break
+        if stadiumName == "MON":
+            stadiumName = "Montreal"
+        else:
+            df = pd.read_csv("../minnemudac github/2023_MLBSchedule.csv")
+            for index, row in df.iterrows():
+                if row["home_team"] == stadiumName:
+                    stadiumName = row["stadium_name"]
+                    break
     # day is 2pm
     # night 7pm
     if (day_night == "D"):
         timeOfDay = 2
     elif(day_night == "N"):
         timeOfDay = 7
-    geolocator = Nominatim(user_agent="MyApp",timeout=10)
+
     # manual locations (not found in geolocator)
     if stadiumName == "Oriole Park at Camden Yards":
         stadiumName = "Baltimore"
@@ -33,10 +47,19 @@ def historicalHourlyTemp(date: str, stadiumName: str, day_night: str):
         stadiumName = "St. Louis"
     if stadiumName == "loanDepot park":
         stadiumName = "Miami"
-    location = geolocator.geocode(stadiumName)
-    if location is None:
-        print(stadiumName + "geolocation not found")
-        return 0
+    if stadiumName == "Angel Stadium":
+        stadiumName = "Anaheim"
+    if stadiumName == "Coors Field":
+        stadiumName = "Denver"
+    if stadiumName not in locations.keys():
+        geolocator = Nominatim(user_agent="MyApp",timeout=10)
+        location = geolocator.geocode(stadiumName)
+        locations[stadiumName] = location
+    else:
+        location = locations[stadiumName]
+    # if location is None:
+    #     print(stadiumName + "geolocation not found")
+    #     return 0
     month = date[4:6]
     if (month[0] == '0'): ## fixes date formatting
         month = month.replace("0", "")
@@ -55,12 +78,12 @@ def historicalHourlyTemp(date: str, stadiumName: str, day_night: str):
         data = Hourly(location, start, end)
 
         data = data.fetch()
-        print(data)
+        # print(data)
         # print(data)
         if not data.empty:
             hourlyTemp.append(float(data["temp"]))
         current_year += 1
-    print(str(time.time() - start_time) + " time to complete")
+    # print(str(time.time() - start_time) + " time to complete")
 
     # start = datetime(current_year, month, day, hour=timeOfDay)  # year month day hour = x
     # end = datetime(end_year, month, day, hour=timeOfDay)  # year month day hour = x
@@ -87,7 +110,7 @@ def historicalHourlyTemp(date: str, stadiumName: str, day_night: str):
     if len(hourlyTemp) == 0:
         return 0
     hourlyTempAvgOnDay = float(sum(hourlyTemp) / len(hourlyTemp)) # gets the mean temp at a certain hour of the year over many years
-    print(hourlyTempAvgOnDay)
+    # print(hourlyTempAvgOnDay)
     return hourlyTempAvgOnDay # in celsius °C
 
     #plot code
@@ -95,19 +118,24 @@ def historicalHourlyTemp(date: str, stadiumName: str, day_night: str):
     # plt.show()
 
 # print(historicalHourlyTemp(str(20010402), "BAL", "D"))
-df = pd.read_csv("fullgl.csv")
-df.insert(1, column="HistoricalAvgHrlyTemp", value="")
+df = pd.read_csv("fileoutput3.csv")
+# df.insert(1, column="HistoricalAvgHrlyTemp", value="")
 for index, row in df.iterrows():
-    if index > 1000:
-        break
-    # if row["Date"] not in seen_gamedates.keys():
-    output_val = historicalHourlyTemp(str(row["Date"]), row["HomeTeam"], row["DayNight"])
-    df.at[index,"HistoricalAvgHrlyTemp"] = output_val
+    if 1793 <= index:
+        try:
+            output_val = historicalHourlyTemp(str(row["Date"]), row["HomeTeam"], row["DayNight"])
+            df.at[index, "HistoricalAvgHrlyTemp"] = output_val
+            # print(index)
+        except Exception as e:
+            print(e)
+            df.to_csv("fileoutput3.csv", index=False)
+            break
+
     # seen_gamedates[row["Date"]] = output_val
     # else:
     #     df.at[index,"HistoricalAvgHrlyTemp"] = seen_gamedates[row["Date"]]
     # print(seen_gamedates)
-df.to_csv("fileoutput.csv")
+df.to_csv("outputs2.csv", index = False)
 # print(df)
 
 

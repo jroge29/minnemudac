0b0e17904bc7ad8e5007c0d95605b33d2cb4d6b8
Kennedy Johnson <joh20327@umn.edu>
2023-03-19 23:44:15 -0500

    weather data final py file

--- weatherData.py
+++ weatherData.py
@@ -1,3 +1,5 @@
+import math
+
 from geopy.geocoders import Nominatim
 from datetime import datetime
 from meteostat import Hourly, Point
@@ -7,15 +9,16 @@ import time
 from tqdm import tqdm
 
 # geolocator = Nominatim(user_agent="MyApp",timeout=10)
-# location = geolocator.geocode("Lakewood")
-# print(location)
+# location = geolocator.geocode("Seattle")
 # location = Point(location.latitude, location.longitude, 70) # 3rd param is altitude
-# start = datetime(2006, 10, 4, hour=2)  # year month day hour = x
-# end = datetime(2006, 10, 4, hour=2)  # year month day hour = x
+#
+# start = datetime(2005, 4, 3, hour=2)  # year month day hour = x
+# end = datetime(2005, 4, 3, hour=2)  # year month day hour = x
 # data = Hourly(location, start, end)
-# #
 # data = data.fetch()
-# print(data)
+# # print(data)
+# print(data.iloc[:, 0])
+# data.to_csv("testing.csv")
 
 def historicalHourlyTemp(date: str, stadiumName: str, day_night: str):
     start_time = time.time()
@@ -26,7 +29,7 @@ def historicalHourlyTemp(date: str, stadiumName: str, day_night: str):
         if stadiumName == "MON":
             stadiumName = "Montreal"
         else:
-            df = pd.read_csv("../minnemudac github/2023_MLBSchedule.csv")
+            df = pd.read_csv("2023_MLBSchedule.csv")
             for index, row in df.iterrows():
                 if row["home_team"] == stadiumName:
                     stadiumName = row["stadium_name"]
@@ -69,74 +72,108 @@ def historicalHourlyTemp(date: str, stadiumName: str, day_night: str):
         day = day.replace("0", "")
     day = int(day)
     location = Point(location.latitude, location.longitude, 70) # 3rd param is altitude
-    hourlyTemp = []
-    current_year = 2005
-    end_year = 2023
-    while current_year != end_year:
-        start = datetime(current_year, month, day, hour=timeOfDay)  # year month day hour = x
-        end = datetime(current_year, month, day, hour=timeOfDay)  # year month day hour = x
-        data = Hourly(location, start, end)
+    current_year = 1973
+    end_year = 2022
 
-        data = data.fetch()
-        # print(data)
-        # print(data)
-        if not data.empty:
-            hourlyTemp.append(float(data["temp"]))
-        current_year += 1
+    start = datetime(current_year, 1, 1, hour=timeOfDay)  # year month day hour = x
+    end = datetime(end_year, 12, 31, hour=timeOfDay)  # year month day hour = x
+    data = Hourly(location, start, end)
+    data = data.fetch()
+
+
     # print(str(time.time() - start_time) + " time to complete")
 
-    # start = datetime(current_year, month, day, hour=timeOfDay)  # year month day hour = x
-    # end = datetime(end_year, month, day, hour=timeOfDay)  # year month day hour = x
-    # data = Hourly(location, start, end)
-    # data = data.fetch()
-    # # slower approach?
-    # hour_counter = 0
-    # year_counter = 0
-    # total_hours = (end_year - current_year + 1) * 24 * 365
-    # while hour_counter <= total_hours: ## ends when 2023 ends
-    #     try:
-    #         hourlyTemp.append(float(data.iloc[hour_counter]["temp"]))
-    #     except IndexError: ## might want to remove this?
-    #         print("no data")
-    #         return 0
-    #     # print(data.iloc[hour_counter]) # for time validation
-    #     if (((year_counter + 1) % 4 == 0) and (year_counter != 0)): ## for leap years
-    #         hour_counter += 8784 # 8784 hrs in a leap year
-    #     else:
-    #         hour_counter += 8760 #8760 hrs in a year
-    #     year_counter += 1
-    # print(str(time.time() - start_time) + " time to complete")
 
-    if len(hourlyTemp) == 0:
-        return 0
-    hourlyTempAvgOnDay = float(sum(hourlyTemp) / len(hourlyTemp)) # gets the mean temp at a certain hour of the year over many years
-    # print(hourlyTempAvgOnDay)
-    return hourlyTempAvgOnDay # in celsius °C
 
-    #plot code
-    # data.plot(y=['tavg', 'tmin', 'tmax'])
-    # plt.show()
+    return data
+
 
 # print(historicalHourlyTemp(str(20010402), "BAL", "D"))
-df = pd.read_csv("fileoutput3.csv")
-# df.insert(1, column="HistoricalAvgHrlyTemp", value="")
+df = pd.read_csv("fullgl.csv")
+df.insert(1, column="HistoricalAvgHrlyTemp", value="")
+seen_teams = []
+db_lst = []
 for index, row in df.iterrows():
-    if 1793 <= index:
-        try:
-            output_val = historicalHourlyTemp(str(row["Date"]), row["HomeTeam"], row["DayNight"])
-            df.at[index, "HistoricalAvgHrlyTemp"] = output_val
-            # print(index)
-        except Exception as e:
-            print(e)
-            df.to_csv("fileoutput3.csv", index=False)
-            break
+            if row["HomeTeam"] not in seen_teams:
+                output_data = historicalHourlyTemp(str(row["Date"]), row["HomeTeam"], row["DayNight"])
+                seen_teams.append(row["HomeTeam"])
+                db_lst.append(output_data)
+            elif row["HomeTeam"] in seen_teams:
+                output_data = db_lst[seen_teams.index(row["HomeTeam"])]
+            hourlyTemp = []
+            current_year = 1973
+            end_year = 2022
+            hour_counter = 0
+            year = int(str(row["Date"])[:4])
+            month = str(row["Date"])[4:6]
+            if (month[0] == '0'):  ## fixes date formatting
+                month = month.replace("0", "")
+            month = int(month)
+            day = str(row["Date"])[6:8]
+            if (day[0] == '0'):  ## fixes date formatting
+                day = day.replace("0", "")
+            day = int(day)
+            # print(year)
+            # hour_counter = 8784  # 8784 hrs in a leap year
+              ## for leap years
+            if month == 1:
+                hour_counter = 24 * day
+            if month == 2:
+                hour_counter = 24 * day + (31 * 24)
+            if month == 3:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24)
+            if month == 4:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24) + (31 * 24)
+            if month == 5:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24) + (31 * 24) + (30 * 24)
+            if month == 6:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24) + (31 * 24) + (30 * 24) + (31 * 24)
+            if month == 7:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (30 * 24)
+            if month == 8:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (30 * 24) + (31 * 24)
+            if month == 9:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (31 * 24)
+            if month == 10:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (31 * 24) + (30 * 24)
+            if month == 11:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (31 * 24) + (30 * 24) + (31 * 24)
+            if month == 12:
+                hour_counter = 24 * day + (31 * 24) + (28 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (31 * 24) + (30 * 24) + (31 * 24) + (30 * 24)
+            if year % 4 == 0:
+                print("here")
+                hour_counter += 24
+            year_counter = 0
+            # total_hours = (end_year - current_year -1) * 24 * 365
+            # print(total_hours)
+            if type(output_data) != int:
+                # print((output_data.shape[0]-1))
+                while hour_counter <= (output_data.shape[0]-1):  ## ends when 2023 ends
+                    # print(output_data)
+                    # print(hour_counter)
+                    if type(output_data) == int:
+                        break
+                    # print(output_data.iloc[[hour_counter]])
+                    # print(row["Date"])
+                    hourlyTemp.append(float(output_data.iat[hour_counter, 0]))
+                    # print(data.iloc[hour_counter]) # for time validation
+                    if (((year_counter+1) % 4 == 0)):  ## for leap years
+                        hour_counter += 8784  # 8784 hrs in a leap year
+                    else:
+                        hour_counter += 8760  # 8760 hrs in a year
+                    year_counter += 1
+                if len(hourlyTemp) == 0:
+                    df.at[index, "HistoricalAvgHrlyTemp"] = 0
+                else:
+                    hourlyTemp = [x for x in hourlyTemp if str(x) != 'nan']
+                    hourlyTempAvgOnDay = float(sum(hourlyTemp) / len(hourlyTemp))
+                    df.at[index, "HistoricalAvgHrlyTemp"] = hourlyTempAvgOnDay
+            else:
+                df.at[index, "HistoricalAvgHrlyTemp"] = 0
+            # print(hourlyTemp)
+            print(index)
 
-    # seen_gamedates[row["Date"]] = output_val
-    # else:
-    #     df.at[index,"HistoricalAvgHrlyTemp"] = seen_gamedates[row["Date"]]
-    # print(seen_gamedates)
 df.to_csv("outputs2.csv", index = False)
-# print(df)
 
 
 
